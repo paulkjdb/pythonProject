38f010401e0f745fc2ba1e9cd9fbd36a7f94184a
paulkjdb <118433690+paulkjdb@users.noreply.github.com>
2022-11-20 18:39:18 +0300

    not good, mistake

--- 01_os_info.py
+++ 01_os_info.py
@@ -1,9 +1,8 @@
 import platform
 import sys
 
-Import ...
-    info = 'OS info is \n {} \n\nPython version is {}{}'.format (
+info = 'OS info is \n {} \n\nPython version is {}{}'.format (
         platform.uname(),sys.version, platform.architecture())
-    )
+
 with open('os info.txt','w') as ff:
     ff.write(info)
